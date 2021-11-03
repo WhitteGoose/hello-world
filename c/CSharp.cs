8a3a8abf3e220810f1762c2b265e75a56575779d
WhitteGoose <89806596+WhitteGoose@users.noreply.github.com>
2021-11-03 15:37:26 +0300

    CSharp.cs
    C# "Hello World" code.
    CSharp "Hello World" code.

--- c/CSharp.cs
+++ c/CSharp.cs
@@ -1,1 +1,10 @@
-System.Console.WriteLine("Hello World");
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
